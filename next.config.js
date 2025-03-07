6dce84f35802813d0b980dd306e4101054d35dd1
David Hamilton <davidhamilton@macbookpro.mynetworksettings.com>
2025-03-07 15:01:45 -0500

    Add Vercel deployment configuration

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "i.ytimg.com",
+        protocol: "https",
+      },
+      {
+        hostname: "yt3.ggpht.com",
+        protocol: "https",
+      },
+      {
+        hostname: "sincere-ferret-226.convex.cloud",
+        protocol: "https",
+      },
+      {
+        hostname: "sincere-ferret-226.convex.site",
+        protocol: "https",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
